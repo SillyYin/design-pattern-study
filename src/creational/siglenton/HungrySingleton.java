c623f469f5719a9150328717fd1a4fa28dad7761
YinRJ <yin_rj@qq.com>
2021-08-03 11:03:15 +0800

    【单例模式】 序列化和反序列化会破坏单例模式

--- src/creational/siglenton/HungrySingleton.java
+++ src/creational/siglenton/HungrySingleton.java
@@ -1,12 +1,14 @@
 package creational.siglenton;
 
+import java.io.Serializable;
+
 /**
  * @author yinrongjie
  * @version 1.0
  * @description 饿汉式
  * @date 2021/8/2
  */
-public class HungrySingleton {
+public class HungrySingleton implements Serializable {
     private final static HungrySingleton instance = new HungrySingleton();
 
     private HungrySingleton() {
--- src/creational/siglenton/Test.java
+++ src/creational/siglenton/Test.java
@@ -1,5 +1,7 @@
 package creational.siglenton;
 
+import java.io.*;
+
 /**
  * @author yinrongjie
  * @version 1.0
@@ -7,11 +9,17 @@ package creational.siglenton;
  * @date 2021/8/2
  */
 public class Test {
-    public static void main(String[] args) {
-        Thread t1 = new Thread(new T());
-        Thread t2 = new Thread(new T());
-        t1.start();
-        t2.start();
-        System.out.println("end");
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        HungrySingleton instance = HungrySingleton.getInstance();
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("singleton_file"));
+        oos.writeObject(instance);
+
+        File file = new File("singleton_file");
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file));
+
+        HungrySingleton newInstance = (HungrySingleton) ois.readObject();
+        System.out.println(instance);
+        System.out.println(newInstance);
+        System.out.println(instance == newInstance);
     }
 }
